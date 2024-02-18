0150bc96afd9da5b5d372d5f83645e7c46280ebc
ERL67 <erl67@pitt.edu>
2024-02-17 21:01:00 -0500

    init

---
+++ Project1/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# start ssh server
+#/etc/init.d/ssh start
+
+# format namenode
+$HADOOP_HOME/bin/hdfs namenode -format
+
+#start hadoop
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+tail -f /dev/null
