47df2ecede453884e454c786a46d041d4de531d0
ERL67 <erl67@users.noreply.github.com>
2024-02-21 20:38:58 -0500

    Delete Project1/code/1.sh

--- Project1/code/1.sh
+++
@@ -1,8 +0,0 @@
-../hadoop/bin/hadoop jar /opt/hadoop/share/hadoop/tools/lib/hadoop-streaming*.jar \
-    -files mapper1.py,reducer1.py \
-    -input input/access_log \
-    -output output/output1 \
-    -mapper /opt/code/mapper1.py \
-    -reducer /opt/code/reducer1.py
-hdfs dfs -cat output/output1/* | tee output1.txt
-hdfs dfs -rm -r output/output1
