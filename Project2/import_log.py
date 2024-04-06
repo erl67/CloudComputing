f8cdb1e506333c2ee911bb7fc6fdc0420d9acd83
ERL67 <erl67@pitt.edu>
2024-04-06 11:27:54 -0400

    fix

--- Project2/import_log.py
+++ Project2/import_log.py
@@ -2,6 +2,7 @@
 
 from cassandra.cluster import Cluster
 from datetime import datetime
+import sys
 
 cluster = Cluster(['127.0.0.1'])
 session = cluster.connect('resized_log_keyspace')
@@ -10,6 +11,9 @@ i = 0
 with open('resized_log', 'r') as f:
     for line in f:
         parts = line.split(' ')
+
+        #print(parts)
+
         ip_address = parts[0]
 
         date_time = datetime.strptime(parts[3][1:], '%d/%b/%Y:%H:%M:%S')
@@ -18,29 +22,34 @@ with open('resized_log', 'r') as f:
 
         requested_url = parts[6]
 
-        http_version = parts[7][:-1]
+        http_version = parts[7][:-1].rstrip('"')
 
         status_code = int(parts[8])
 
         try:
             response_size = int(parts[9])
         except:
-            response_size = None
-        user_agent = ' '.join(parts[11:]).strip()[1:-2]
+            response_size = 0
+
+        user_agent = ' '.join(parts[11:]).strip()[1:-5]
 
         query = "INSERT INTO log_table (ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
 
         if i % 10000 == 0:
-            print(f"{i=}\t{query}")
+            print(f"{i=}\n{query}")
             print(ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent)
 
         try:
+            pass
             session.execute(query, (ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent))
         except:
             print(f"{line=}")
             print(f"{i=}\t{query=}")
             print(ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent)
 
+        i += 1
+        #if i > 100:
+        #    sys.exit(1)
 
 
 cluster.shutdown()
