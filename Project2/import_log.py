7d95d37923b030918f27d9d59a2e56fac84a43c9
ERL67 <erl67@pitt.edu>
2024-04-06 10:58:02 -0400

    fix import

--- Project2/import_log.py
+++ Project2/import_log.py
@@ -5,29 +5,42 @@ from datetime import datetime
 
 cluster = Cluster(['127.0.0.1'])
 session = cluster.connect('resized_log_keyspace')
+i = 0
 
 with open('resized_log', 'r') as f:
     for line in f:
         parts = line.split(' ')
         ip_address = parts[0]
+
         date_time = datetime.strptime(parts[3][1:], '%d/%b/%Y:%H:%M:%S')
+
         request_method = parts[5][1:]
+
         requested_url = parts[6]
+
         http_version = parts[7][:-1]
+
         status_code = int(parts[8])
+
         try:
             response_size = int(parts[9])
         except:
             response_size = None
         user_agent = ' '.join(parts[11:]).strip()[1:-2]
 
-        query = """
-            INSERT INTO log_table (ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent)"
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            """
-        #print(query)
-        print(ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent)
+        query = "INSERT INTO log_table (ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
 
-        session.execute(query, (ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent))
+        if i % 10000 == 0:
+            print(f"{i=}\t{query}")
+            print(ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent)
+
+        try:
+            session.execute(query, (ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent))
+        except:
+            print(f"{line=}")
+            print(f"{i=}\t{query=}")
+            print(ip_address, date_time, request_method, requested_url, http_version, status_code, response_size, user_agent)
+
+
 
 cluster.shutdown()
